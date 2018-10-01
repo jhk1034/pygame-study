dc5149ed00962b41de81c4bf76d5a9f3812d7200
jhk1034 <43343575+jhk1034@users.noreply.github.com>
2018-10-01 23:10:48 +0900

    Add files via upload

--- mainloop.py
+++ mainloop.py
@@ -8,11 +8,15 @@ keys = [False, False, False, False]
 playerpos = [100, 100]
 
 screen = pygame.display.set_mode((width, height))
+acc = [0, 0] #[적을 몇명 죽였는지,화살을 몇개를 쐈는지]
+arrows = []
 
 #3. 이미지를 가져온다.
-player = pygame.image.load("images/dude.png")
-grass = pygame.image.load("images/grass.png")
-castle = pygame.image.load("images/castle.png")
+player = pygame.image.load("resources/images/dude.png")
+grass = pygame.image.load("resources/images/grass.png")
+castle = pygame.image.load("resources/images/castle.png")
+arrow = pygame.image.load("resources/images/bullet.png")
+
 #4. 계속 화면이 보이도록 한다.
 
 while True:
@@ -31,17 +35,36 @@ while True:
     screen.blit(castle, (0, 240))
     screen.blit(castle, (0, 345))
 
-    screen.blit(player,playerpos) #screen이라는 객체에 blit함수를 가져온다.
+    # screen.blit(player,playerpos) #screen이라는 객체에 blit함수를 가져온다.
 
-    #6-1 캐릭터 회전
-    position = pygame.mouse.get_pos() #현재 마우스의 위치 값
-    angle = math.atan2(position[1]-(playerpos[1]+32), position[0]-(playerpos[0]+26))
-    playerrot = pygame.transform.rotate(player, 360-angle * 57.29) #마우스 위치로 일정하게 회전
+    #6.1 캐릭터 회전
+    position = pygame.mouse.get_pos() #현재 마우스의 위치 값을 찾아냄
+    angle = math.atan2(position[1]-(playerpos[1]+32), position[0]-(playerpos[0]+26)) #+32, +26는 토끼의 중심에서 회전하기 위함
+    playerrot = pygame.transform.rotate(player, 360-angle * 57.29) #마우스 위치로 일정하게 회전 2pi=360
     playerpos1 = (playerpos[0]-playerrot.get_rect().width//2, playerpos[1]-playerrot.get_rect().height//2)
     screen.blit(playerrot, playerpos1)
+    
+    #6.2  화살 그리기
+    for bullet in arrows: #bullet <== [각도, 플레이어의 x좌표, 플레이어의 y좌표]
+        index=0
+        velx = math.cos(bullet[0]) * 10
+        vely = math.sin(bullet[0]) * 10
 
-    #7. 화면을 다시 그린다.
-    pygame.display.flip()#화면을 업데이트 = pygame.display.updata()
+        bullet[1]+=velx
+        bullet[2]+=vely
+
+        if bullet[1] < -64 or bullet[1]>640 or bullet[2]< -60 or bullet[2] > 480:
+            arrows.pop(index)
+
+        index += 1
+
+    for projectile in arrows:
+        arrow1 = pygame.transform.rotate(arrow, 360 - projectile[0] * 57.29)
+        screen.blit(arrow1, (projectile[1], projectile[2]))
+    
+    
+    #7. 화면을 다시 그린다.\
+    pygame.display.flip() #display.update
 
     #8 게임을 종료
     for event in pygame.event.get():
@@ -72,6 +95,22 @@ while True:
             elif event.key == pygame.K_d:
                 keys[3] = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            acc[1] = acc[1] + 1
+            arrows.append([math.atan2(position[1]-(playerpos1[1]+32), \
+                           position[0] - (playerpos1[0]+26)),\
+                           playerpos1[0]+32,\
+                           playerpos1[1]+32])
+
+
+#arrows 화살이 나갈 angle
+
+
+    playerpos1[0] + 32
+    playerpos1[1]+32
+
+    #마우스 이벤트
     if keys[0]:
         playerpos[1] -= 5
     elif keys[2]:
